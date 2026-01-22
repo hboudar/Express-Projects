1dbe288b1fa7dd26ad5b5283ff12192c81665ab6
hboudar <boudarhamza12@gmail.com>
2026-01-22 18:44:04 +0100

    mdf in app.js : instead of xss package switched to xss-clean package, also pushing the final folder

--- 04-jobs-api/starter/app.js
+++ 04-jobs-api/starter/app.js
@@ -7,10 +7,8 @@ const app = express();
 //extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
-const xss = require('xss')
-const rateLimiter = require('express-rate-limit')
-
-
+const xss = require('xss-clean')
+const {rateLimit} = require('express-rate-limit');
 
 //connectDB & middlewares
 const connectDB = require('./db/connect');
@@ -25,18 +23,22 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1)
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000, //15 min
-  max: 100, //limit for each ip to 100 req per windowMs
-}));
 
-app.use(express.json());
-app.use(helmet())
-app.use(cors())
-app.use(xss())
 
 // extra packages
+app.use(express.json());//not extra : for req.body (post req)
+app.set('trust proxy', 1)
+
+app.use(helmet())
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}));
+
+app.use(xss());
+app.use(cors())
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
