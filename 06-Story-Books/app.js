3473d277cac36b9c7e2fb56b33227c5bb930c1e1
hboudar <boudarhamza12@gmail.com>
2026-01-24 17:16:19 +0100

    add basic hbs rendering & css in views & public

--- 06-Story-Books/app.js
+++ 06-Story-Books/app.js
@@ -1,12 +1,30 @@
+const path = require('path')
 const dotenv = require('dotenv')
 const express = require('express')
 const connectDb = require('./database/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 
 dotenv.config({path: './config/config.env'})
 
 connectDb();
-
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+
 
 const port = process.env.PORT || 5000;
 
