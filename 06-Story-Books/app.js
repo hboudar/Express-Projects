6f2b86b28c29488a6b7a82da2fa87ec18b6f6bdd
hboudar <boudarhamza12@gmail.com>
2026-01-25 14:20:33 +0100

    adding a couple more routes for handling stories : adding, editing and deleting and more

--- 06-Story-Books/app.js
+++ 06-Story-Books/app.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const dotenv = require('dotenv')
 const express = require('express')
-const connectDb = require('./database/db')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo').default
+const connectDb = require('./database/db')
 
 // Load config
 dotenv.config({path: './config/config.env'})
@@ -16,25 +19,55 @@ require('./config/passport')(passport)
 connectDb();
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            let method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    })
+);
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs.engine({ 
+    defaultLayout: 'main', extname: '.hbs', helpers: { formatDate, stripTags, truncate, editIcon, select } }));
 app.set('view engine', '.hbs');
 
 // Sessions
-app.use(session({
+app.use(
+  session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+)
 
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,6 +75,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 
 const port = process.env.PORT || 5000;
